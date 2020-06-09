1d3be68daf63fe14df6272f6b6e4bdafcb464652
teodoc <s.v.minckwitz@gmail.com>
2020-06-09 22:39:13 +0200

    add readme

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 //Puzzle
-const size = parseInt(getUrlParam('size',9)); //1,4,9
-var sqrt_size = Math.sqrt(tsize);
+const size = parseInt(getUrlParam('size',3)); 
 
 var mpuzzle = [], tpuzzle = [];
 const drawsize=parseInt(getUrlParam('ds',100));
@@ -13,7 +12,8 @@ var canvaslist = [], mouseover = []
 var solutions = [];
 var count = 0;
 var time = 0;
-var tsize = size;
+
+var chars = "abcdefghijklmnopqrstuvwxyz0123456789".split('');
 
 //Puzzle
 mpuzzle = getUrlParam("puzzle",0);
@@ -36,12 +36,14 @@ function Puzzle(p) {
 
 function prep() {
   let p = new Puzzle([]);
-  tpuzzle = compress(mpuzzle);
-  compress(mpuzzle,false).forEach(part => { 
-    p.puzzle[0] = part;
-    t = new Puzzle(copy(p.puzzle));
-    p.childs.push(t);
-  });
+  if(mpuzzle.length >= size*size) {
+    tpuzzle = compress(mpuzzle);
+    compress(mpuzzle,false).forEach(part => { 
+      p.puzzle[0] = part;
+      t = new Puzzle(copy(p.puzzle));
+      p.childs.push(t);
+    });
+  }
   return p;
 }
 
@@ -50,7 +52,7 @@ function solve(c, i=0) {
   c.childs.forEach(c => {
     if(solutions.length < maxsolutions && count < maxchecks)  {
       fill(c,i);
-      if (i <= tsize -1) {
+      if (i <= (size*size) -1) {
         solve(c, i); 
       } else {
         solutions.push(c.puzzle);        
@@ -95,7 +97,7 @@ function check(puzzle, n) {
   let a = 0, b = 0,l=puzzle.length;
 
   //top
-  a=n-sqrt_size;
+  a=n-size;
   a = a >= 0 ? puzzle[a][3] : 0;
   b= puzzle[n][1];
   if (a !== 0 && b !== 0) {
@@ -104,14 +106,14 @@ function check(puzzle, n) {
 
   //left
   a=n-1;
-  a = ((a >= 0) && (n%sqrt_size !=0)) ? puzzle[a][2] : 0;
+  a = ((a >= 0) && (n%size !=0)) ? puzzle[a][2] : 0;
   b= puzzle[n][4];
   if (a !== 0 && b !== 0) {
     if ((a + b) != 0) { return false; }
   }
   
   //bottom
-  a=n+sqrt_size;
+  a=n+size;
   a = a < l ? puzzle[a][1] : 0;
   b= puzzle[n][3];
   if (a !== 0 && b !== 0) {
@@ -120,7 +122,7 @@ function check(puzzle, n) {
 
   //right
   a=n+1;
-  a = ((a < l)  && (n%sqrt_size !=sqrt_size)) ? puzzle[a][4] : 0;
+  a = ((a < l)  && (n%size !=size)) ? puzzle[a][4] : 0;
   b= puzzle[n][2];
   if (a !== 0 && b !== 0) {
     if ((a + b) != 0) { return false; }
@@ -175,42 +177,38 @@ function compress(puzzle,rotate=true) {
 
 function init() {
   div = document.getElementById('header');
-  tsize = 1;  sqrt_size = Math.sqrt(tsize);
 
   let puzzle = []
   mpuzzle.forEach( p => {puzzle.push([p])})
 
   puzzle.forEach(p => {
     div.insertAdjacentHTML('beforeend', '<canvas id="can' + cancount +
-                           '" width="' + (sqrt_size * drawsize) + 
-                           '" height="' + (sqrt_size * drawsize) + '"></canvas>');
+                           '" width=100"' + 
+                           '" height=100></canvas>');
     canvaslist[cancount] = document.getElementById("can" + (cancount));
 
     canvaslist[cancount].i = cancount;
     canvaslist[cancount].onclick = function(event) {
-      if((event.y-this.getBoundingClientRect().y ) < ((drawsize+30)/3)) {
+      if((event.y-this.getBoundingClientRect().y ) < ((100+30)/3)) {
         mpuzzle[this.i][1] = next(mpuzzle[this.i][1]);
       }
-      if((event.y-this.getBoundingClientRect().y ) > (drawsize+30)-((drawsize+30)/3)) {
+      if((event.y-this.getBoundingClientRect().y ) > (100+30)-((100+30)/3)) {
         mpuzzle[this.i][3] = next(mpuzzle[this.i][3]);
       }
-      if((event.x-this.getBoundingClientRect().x ) < ((drawsize+30)/3)) {
+      if((event.x-this.getBoundingClientRect().x ) < ((100+30)/3)) {
         mpuzzle[this.i][4] = next(mpuzzle[this.i][4]);
       }
-      if((event.x-this.getBoundingClientRect().x ) > (drawsize+30)-((drawsize+30)/3)) {
+      if((event.x-this.getBoundingClientRect().x ) > (100+30)-((100+30)/3)) {
         mpuzzle[this.i][2] = next(mpuzzle[this.i][2]);
       }
-      tsize = 1;
-      drawPuzzle(p, canvaslist[this.i]);
-      tsize = size; sqrt_size = Math.sqrt(tsize); 
+      drawPuzzle(p, canvaslist[this.i],1,100);
       main();
     }
-    drawPuzzle(p, canvaslist[cancount]);
+    drawPuzzle(p, canvaslist[cancount],1,100);
     cancount++;    
   });
 
   div.insertAdjacentHTML('beforeend', '<br>');
-  tsize = size; sqrt_size = Math.sqrt(tsize); 
 }
 
 function main(){
@@ -229,8 +227,6 @@ function main(){
 }
 
 function createCanvases() {
-  tsize = size;
-  sqrt_size = Math.sqrt(tsize);
   let div = document.getElementById('solutions');
   let padding = Math.floor(15*drawsize/100);
 
@@ -239,8 +235,8 @@ function createCanvases() {
 
   for(let i=0; i<solutions.length; i++){
     div.insertAdjacentHTML('beforeend', '<canvas style="padding:'+padding+'px;"' +
-                           'id="can' + cancount + '" width="' + (sqrt_size * drawsize) + 
-                           '" height="' + (sqrt_size * drawsize) + '"></canvas>');
+                           'id="can' + cancount + '" width="' + (size * drawsize) + 
+                           '" height="' + (size * drawsize) + '"></canvas>');
     canvaslist[cancount] = document.getElementById("can" + (cancount));
     cancount++;
   }
@@ -248,15 +244,17 @@ function createCanvases() {
 
 
 function drawHeader() {
-  let link = "index.html?puzzle="+btoa(JSON.stringify(mpuzzle))+"&size="+size;
-  let pluslink = "index.html?puzzle="+btoa(JSON.stringify(addPiece()))+"&size="+size;
-  let minuslink = "index.html?puzzle="+btoa(JSON.stringify(removePiece()))+"&size="+size;
-  let rndlink = "index.html?puzzle="+btoa(JSON.stringify(randomPuzzle()))+"&size="+size;
-  let emptylink = "index.html?puzzle="+btoa(JSON.stringify(emptyPuzzle()))+"&size="+size;
-  let resetlink = "index.html?puzzle="+btoa(JSON.stringify(stdPuzzle()))+"&size=9";
+  let param = "&size="+size+"&ds="+drawsize+"&max="+maxsolutions+"&"
+  let link = "index.html?"+param+"puzzle="+btoa(JSON.stringify(mpuzzle));
+  let pluslink = "index.html?"+param+"puzzle="+btoa(JSON.stringify(addPiece()));
+  let minuslink = "index.html?"+param+"puzzle="+btoa(JSON.stringify(removePiece()));
+  let rndlink = "index.html?"+param+"puzzle="+btoa(JSON.stringify(randomPuzzle()));
+  let emptylink = "index.html?"+param+"puzzle="+btoa(JSON.stringify(emptyPuzzle()));
+  let resetlink = "index.html?size=3&puzzle="+btoa(JSON.stringify(stdPuzzle()));
+  let solutionstext = (solutions.length == maxsolutions || count >= maxchecks)  ? solutions.length+"+" : solutions.length;
 
   document.getElementById('text').innerHTML = "<p>C:" + count + "  " +
-    (time + " ms ") + solutions.length+ " Lösungen </p><p>" +
+    (time + " ms <b>") + solutionstext+ "</b> Lösungen </p><p>" +
     "<a href="+link+" class='link-button'>Puzzle Link</a>" +
     "<a href="+pluslink+" class='link-button'>+</a>" +
     "<a href="+minuslink+" class='link-button'>-</a>" +
@@ -268,147 +266,148 @@ function drawHeader() {
 function drawSolutions() {
   i=mpuzzle.length;
   solutions.forEach(p => {       
-    if(canvaslist.length > i) { drawPuzzle(p, canvaslist[i]); }
+    if(canvaslist.length > i) { drawPuzzle(p, canvaslist[i],size,drawsize); }
     i++;
   });
 }
 
-function drawPuzzle(puzzle, can) {
+//puzzle, canvas, size, drawsize
+function drawPuzzle(puzzle, can, s ,ds) {
   const color = ["rgb(240,240,240)", "rgb(255,255,0)", "rgb(255,130,0)",
                  "rgb(80,190,80)", "rgb(0,80,255)"];
   let ctx = can.getContext("2d");
-  let s = drawsize/100; //scale
+  let f = ds/100; //scale
   let x = 0;
   let y = 0;
 
   //Clear
-  ctx.lineWidth = Math.floor(1*s)
+  ctx.lineWidth = Math.floor(1*f)
   
   ctx.fillStyle = "#FFFFFF";
-  ctx.fillRect(0, 0, sqrt_size * drawsize, sqrt_size * drawsize);
-  ctx.rect(0, 0, sqrt_size * drawsize, sqrt_size * drawsize);
+  ctx.fillRect(0, 0,s * ds,s * ds);
+  ctx.rect(0, 0,s * ds,s * ds);
   ctx.stroke();
 
   //Grid
-  for (let i = 0; i < sqrt_size - 1; i++) {
-    ctx.moveTo(0, drawsize + i * drawsize);
-    ctx.lineTo(sqrt_size * drawsize, drawsize + i * drawsize);
+  for (let i = 0; i < s - 1; i++) {
+    ctx.moveTo(0, ds + i * ds);
+    ctx.lineTo(size * ds, ds + i * ds);
     ctx.stroke();
-    ctx.moveTo(drawsize + i * drawsize, 0);
-    ctx.lineTo(drawsize + i * drawsize, sqrt_size * drawsize);
+    ctx.moveTo(ds + i * ds, 0);
+    ctx.lineTo(ds + i * ds,s * ds);
     ctx.stroke();
   }
 
-  for (let i = 0; i < tsize; i++) {
+  for (let i = 0; i < (s*s); i++) {
 
     ctx.fillStyle = "#000000";
-    ctx.font = Math.floor((30-puzzle[i][0].length*3)*s)+"px Arial";
-    x = (i % sqrt_size) * drawsize;
-    y = Math.floor(i / sqrt_size) * drawsize
-    ctx.fillText(puzzle[i][0], 41*s + x-puzzle[i][0].length*2, y + 59*s);
+    ctx.font = Math.floor((30-puzzle[i][0].length*2.1)*f)+"px Arial";
+    x = (i %s) * ds;
+    y = Math.floor(i /s) * ds
+    ctx.fillText(puzzle[i][0], x-ctx.measureText(puzzle[i][0]).width/2+ds/2, y + 59*f);
 
     // top
-    x = drawsize/2 + (i % sqrt_size) * drawsize;
-    y = Math.floor(i / sqrt_size) * drawsize;
+    x = ds/2 + (i %s) * ds;
+    y = Math.floor(i /s) * ds;
 
     ctx.fillStyle = color[Math.abs(puzzle[i][1])];
     ctx.beginPath();
-    ctx.arc(x, y, 24*s, 0, Math.PI);
+    ctx.arc(x, y, 24*f, 0, Math.PI);
     ctx.fill();
     ctx.stroke();
 
     ctx.fillStyle = "#000000";
     ctx.beginPath();
     if (puzzle[i][1] < 0) {
-      ctx.arc(x, y, 15*s, 0, 0.5 * Math.PI);
+      ctx.arc(x, y, 15*f, 0, 0.5 * Math.PI);
       ctx.stroke();
       ctx.beginPath();
-      ctx.arc(-9*s + x, 8*s + y, 4*s, 0, 2 * Math.PI);
+      ctx.arc(-9*f + x, 8*f + y, 4*f, 0, 2 * Math.PI);
       ctx.fill();
     } else {
-      ctx.arc(x, y, 15*s, 0.5 * Math.PI, Math.PI);
+      ctx.arc(x, y, 15*f, 0.5 * Math.PI, Math.PI);
       ctx.stroke();
       ctx.beginPath();
-      ctx.arc(+8*s + x, 9*s + y, 4*s, 0, 2 * Math.PI);
+      ctx.arc(+8*f + x, 9*f + y, 4*f, 0, 2 * Math.PI);
       ctx.fill();
     }
 
     // right
-    x = drawsize + (i % sqrt_size) * drawsize;
-    y = drawsize/2 + Math.floor(i / sqrt_size) * drawsize;
+    x = ds + (i %s) * ds;
+    y = ds/2 + Math.floor(i /s) * ds;
 
     ctx.fillStyle = color[Math.abs(puzzle[i][2])];
     ctx.beginPath();
-    ctx.arc(x, y, 24*s, 0.5 * Math.PI, 1.5 * Math.PI);
+    ctx.arc(x, y, 24*f, 0.5 * Math.PI, 1.5 * Math.PI);
     ctx.fill();
     ctx.stroke();
 
     ctx.fillStyle = "#000000";
     ctx.beginPath();
     if (puzzle[i][2] < 0) {
-      ctx.arc(x, y, 15*s, 0.5 * Math.PI, Math.PI);
+      ctx.arc(x, y, 15*f, 0.5 * Math.PI, Math.PI);
       ctx.stroke();
       ctx.beginPath();
-      ctx.arc(-9*s + x, -8*s + y, 4*s, 0, 2 * Math.PI);
+      ctx.arc(-9*f + x, -8*f + y, 4*f, 0, 2 * Math.PI);
       ctx.fill();
     } else {
-      ctx.arc(x, y, 15*s, Math.PI, 1.5 * Math.PI);
+      ctx.arc(x, y, 15*f, Math.PI, 1.5 * Math.PI);
       ctx.stroke();
       ctx.beginPath();
-      ctx.arc(-9*s + x, +8*s + y, 4*s, 0, 2 * Math.PI);
+      ctx.arc(-9*f + x, +8*f + y, 4*f, 0, 2 * Math.PI);
       ctx.fill();
     }
     ctx.stroke();
 
     // bottom
-    x = drawsize/2 + (i % sqrt_size) * drawsize;
-    y = drawsize + Math.floor(i / sqrt_size) * drawsize;
+    x = ds/2 + (i %s) * ds;
+    y = ds + Math.floor(i /s) * ds;
 
     ctx.fillStyle = color[Math.abs(puzzle[i][3])];
     ctx.beginPath();
-    ctx.arc(x, y, 24*s, Math.PI, 2*Math.PI);
+    ctx.arc(x, y, 24*f, Math.PI, 2*Math.PI);
     ctx.fill();
     ctx.stroke();
 
     ctx.fillStyle = "#000000";
     ctx.beginPath();
     if (puzzle[i][3] < 0) {
-      ctx.arc(x, y, 15*s, Math.PI, 1.5 * Math.PI);
+      ctx.arc(x, y, 15*f, Math.PI, 1.5 * Math.PI);
       ctx.stroke();
       ctx.beginPath();
-      ctx.arc(+8*s + x, -9*s + y, 4*s, 0, 2 * Math.PI);
+      ctx.arc(+8*f + x, -9*f + y, 4*f, 0, 2 * Math.PI);
       ctx.fill();
     } else {
-      ctx.arc(x, y, 15*s, 1.5 * Math.PI, 0);
+      ctx.arc(x, y, 15*f, 1.5 * Math.PI, 0);
       ctx.stroke();
       ctx.beginPath();
-      ctx.arc(-8*s + x, -9*s + y, 4*s, 0, 2 * Math.PI);
+      ctx.arc(-8*f + x, -9*f + y, 4*f, 0, 2 * Math.PI);
       ctx.fill();
     }
     ctx.stroke();
 
     // left
-    x = (i % sqrt_size) * drawsize;
-    y = drawsize/2 + Math.floor(i / sqrt_size) * drawsize;
+    x = (i %s) * ds;
+    y = ds/2 + Math.floor(i /s) * ds;
     ctx.fillStyle = color[Math.abs(puzzle[i][4])];
     ctx.beginPath();
-    ctx.arc(x, y, 24*s, 1.5 * Math.PI, 0.5 * Math.PI);
+    ctx.arc(x, y, 24*f, 1.5 * Math.PI, 0.5 * Math.PI);
     ctx.fill();
     ctx.stroke();
 
     ctx.beginPath();
     ctx.fillStyle = "#000000";
     if (puzzle[i][4] < 0) {
-      ctx.arc(x, y, 15*s, 1.5 * Math.PI, 0);
+      ctx.arc(x, y, 15*f, 1.5 * Math.PI, 0);
       ctx.stroke();
       ctx.beginPath();
-      ctx.arc(9*s + x, +8*s + y, 4*s, 0, 2 * Math.PI);
+      ctx.arc(9*f + x, +8*f + y, 4*f, 0, 2 * Math.PI);
       ctx.fill();
     } else {
-      ctx.arc(x, y, 15*s, 0, 0.5 * Math.PI);
+      ctx.arc(x, y, 15*f, 0, 0.5 * Math.PI);
       ctx.stroke();
       ctx.beginPath();
-      ctx.arc(9*s + x, -8*s + y, 4*s, 0, 2 * Math.PI);
+      ctx.arc(9*f + x, -8*f + y, 4*f, 0, 2 * Math.PI);
       ctx.fill();
     }
     ctx.stroke();
@@ -418,15 +417,15 @@ function drawPuzzle(puzzle, can) {
 function checkAll(puzzle, f = 0) {
   count++;
   let a = 0, b = 0,l=puzzle.length;
-  for (let i = 0; i < sqrt_size; i++) {
-    for (let j = 0; j < sqrt_size - 1; j++) {
-      a=j + i * sqrt_size;  b=1 + j + i * sqrt_size;
+  for (let i = 0; i <s; i++) {
+    for (let j = 0; j <s - 1; j++) {
+      a=j + i *s;  b=1 + j + i *s;
       if(a < l) {a = puzzle[a][2]} else {a = 0;}
       if(b < l) {b = puzzle[b][4]} else {b = 0;}
       if (a !== 0 && b !== 0 && f === 0) {
         if ((a + b) != 0) { return false; }
       }
-      a=j * sqrt_size + i; b=(j + 1) * sqrt_size + i;
+      a=j * size + i; b=(j + 1) * size + i;
       if(a < l) {a = puzzle[a][3]} else {a = 0;}
       if(b < l) {b = puzzle[b][1]} else {b = 0;}
       if (a !== 0 && b !== 0 && f === 0) {
@@ -457,9 +456,8 @@ function next(n) {
 function randomPuzzle()
 {
   let puzzle = [] 
-  c = "abcdefghijklmnopqrstuvwxyz0123456789".split('');
   for(let i=0; i<mpuzzle.length; i++){
-    puzzle.push([c[i],getRandom(),getRandom(),getRandom(),getRandom()])
+    puzzle.push([chars[i],getRandom(),getRandom(),getRandom(),getRandom()])
   }
   return puzzle;
 }
@@ -467,9 +465,8 @@ function randomPuzzle()
 function emptyPuzzle()
 {
   let puzzle = [] 
-  let c = "abcdefghijklmnopqrstuvwxyz0123456789".split('');
   for(let i=0; i<mpuzzle.length; i++){
-    puzzle.push([c[i],0,0,0,0])
+    puzzle.push([chars[i],0,0,0,0])
   }
   return puzzle;
 }
@@ -477,7 +474,6 @@ function emptyPuzzle()
 function stdPuzzle()
 {
   let puzzle = [];
-  let c = "abcdefghijklmnopqrstuvwxyz0123456789".split('');
   puzzle.push(["a", 1, -4, -2, 3]);
   puzzle.push(["b", -3, 2, 1, -2]);
   puzzle.push(["c", -1, 3, 4, -2]);
@@ -491,7 +487,7 @@ function stdPuzzle()
   puzzle.push(["i", 4, 1, -2, -3]); 
 
   for (let i=puzzle.length; i<size; i++){
-    puzzle.push([c[i],0,0,0,0])
+    puzzle.push([chars[i],0,0,0,0])
   }
 
   return puzzle;
@@ -499,8 +495,7 @@ function stdPuzzle()
 
 function addPiece() {
   let puzzle = copy(mpuzzle);
-  let c = "abcdefghijklmnopqrstuvwxyz0123456789".split('');
-  puzzle.push([c[puzzle.length],0,0,0,0]);
+  puzzle.push([chars[puzzle.length],0,0,0,0]);
 
   return puzzle
 }
